9689ac259bdfce5961dac30b4e98b36ba05a20b6
Marcel Wysocki <maci@satgnu.net>
2013-06-25 19:37:55 +0200

    stats

--- memdap/stats.sh
+++ memdap/stats.sh
@@ -4,8 +4,9 @@ compr=$(< /sys/block/zram0/compr_data_size)
 orig=$(< /sys/block/zram0/orig_data_size)
 ratio=$(echo "scale=2; $orig*100/$compr" | bc -q)
 
-echo -e "Number of entries:\t `find ldif/ |wc -l`"
+echo -e "Number of entries:\t `find ldif -type f 2>/dev/null | wc -l`"
+echo -e "Free space:\t\t `df -m| grep zram0 |awk '{ print $4 }'` MB"
 echo -e "Uncompressed size:\t $((${orig}/1024/1024)) MB"
 echo -e "Compressed size:\t $((${compr}/1024/1024)) MB"
 echo -e "Compression ratio:\t ${ratio}%"
-echo -e "Fragmentation:\t `xfs_db -c frag -r /dev/zram0`"
+echo -e "Fragmentation:\t\t `xfs_db -c frag -r /dev/zram0`"
