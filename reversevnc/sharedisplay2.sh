da56e2450860219ffd99eef8398d21d8d3faa378
Marcel Wysocki <maci.stgn@gmail.com>
2013-09-26 17:31:39 +0200

    disable clipboard sharing

--- reversevnc/sharedisplay2.sh
+++ reversevnc/sharedisplay2.sh
@@ -7,4 +7,4 @@ ssh display@192.168.168.111 /home/display/killstale.sh
 OUTPUT=`ssh -t display@192.168.168.111 /home/display/vnclistener.sh -FullScreen=1`
 PORT=`echo $OUTPUT | cut -d\: -f2`
 
-x11vnc -repeat -clip 1920x1200+1920+0 -scale 1920x1080 -coe 192.168.168.111:$PORT -timeout 5
+x11vnc -noclipboard -nosetclipboard -repeat -clip 1920x1200+1920+0 -scale 1920x1080 -coe 192.168.168.111:$PORT -timeout 5
