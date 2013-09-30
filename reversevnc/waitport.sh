d7ef8609fe42fa28349a5f5e344b2e37c87fcd5b
Marcel Wysocki <maci.stgn@gmail.com>
2013-09-30 18:06:56 +0200

    less sleeping

--- reversevnc/waitport.sh
+++ reversevnc/waitport.sh
@@ -4,7 +4,7 @@ PORT=$1
 #wait until everything is ready
 while [ -z "`netstat -antp 2>/dev/null | grep vncviewer | grep $PORT`" ];
 do
-  sleep 1
+  sleep 0.1
 done
 
 echo Success, listening on port:${PORT}
