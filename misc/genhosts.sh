936330f9a91f888011098530a08a1cf42ec3f55e
Marcel Wysocki <maci.stgn@gmail.com>
2013-10-09 17:22:35 +0200

    add genhosts

---
+++ misc/genhosts.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+OUTFILE=/etc/hosts
+
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
+for HOST in `host -l  local.domain 192.168.0.1 | grep address`
+do
+  IP_ADDR=`echo $HOST | awk '{ print $(NF) }'`
+  FQDN=`echo $HOST | awk '{ print $(1) }'`
+  SHORTNAME=`echo $FQDN | cut -d\. -f 1`
+
+  printf "%s\t\t%-50s%s\n" "$IP_ADDR" "$FQDN" "$SHORTNAME"
+
+done | sort > $OUTFILE
+
+echo "127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
+::1         localhost localhost.localdomain localhost6 localhost6.localdomain6" >> $OUTPUT
+
+
+
